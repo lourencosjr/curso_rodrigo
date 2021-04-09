523e7d446bb3eed163ccad846c26df54058e4c8c
João Lourenço <lourencosjr@gmail.com>
2021-04-09 10:46:35 -0300

    movendo os detalhes da api da classe de domínio para a classe de data

---
+++ lib/data/usecases/remote_authentication.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../../domain/usecases/usecases.dart';
+
+import '../http/http.dart';
+
+class RemoteAuthentication {
+  final HttpClient httpClient;
+  final String url;
+
+  RemoteAuthentication({@required this.httpClient, @required this.url});
+
+  Future<void> auth(AuthenticationParams params) async {
+    final body = RemoteAuthenticationParams.fromDomain(params).toJson();
+    await httpClient.request(
+      url: url,
+      method: 'post',
+      body: body,
+    );
+  }
+}
+
+class RemoteAuthenticationParams {
+  final String email;
+  final String password;
+
+  RemoteAuthenticationParams({
+    @required this.email,
+    @required this.password,
+  });
+
+  Map toJson() => {'email': email, 'password': password};
+
+  factory RemoteAuthenticationParams.fromDomain(AuthenticationParams params) =>
+      RemoteAuthenticationParams(email: params.email, password: params.secret);
+}
