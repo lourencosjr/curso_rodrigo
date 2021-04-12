10a932403d6a029bbdc50ce71cd12d18767f29cc
João Lourenço <lourencosjr@gmail.com>
2021-04-12 16:57:31 -0300

    garantindo que o HttpAdapter chamou sem o  BODY

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -22,10 +22,13 @@ class HttpAdapter {
       'content-type': 'application;json',
       'accept': 'application;json',
     };
+
+    final jsonBody = body != null ? jsonEncode(body) : null;
+
     await client.post(
       Uri.parse(url),
       headers: headers,
-      body: jsonEncode(body),
+      body: jsonBody,
     );
   }
 }
@@ -58,6 +61,20 @@ void main() {
             'accept': 'application;json',
           },
           body: '{"any_key":"any_value"}',
+        ),
+      );
+    });
+
+    test('Deve chamar o POST sem os valores do BODY', () async {
+      final client = ClientSpy();
+      final sut = HttpAdapter(client);
+      final url = faker.internet.httpUrl();
+      await sut.request(url: url, method: 'post');
+
+      verify(
+        client.post(
+          any,
+          headers: anyNamed('headers'),
         ),
       );
     });
